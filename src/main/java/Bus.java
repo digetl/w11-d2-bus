bdbf183eed6ab28a10e20759efb75cefb3c0e561
digetl <dave@digetl.co.uk>
2020-06-16 14:06:48 +0100

    bus hasName

--- src/main/java/Bus.java
+++ src/main/java/Bus.java
@@ -1,2 +1,17 @@
 public class Bus {
+
+    private String name;
+    private String destination;
+    private int capacity;
+
+    public Bus(String name){
+        this.name = name;
+//        this.destination = destination;
+//        this.capacity = capacity;
+    }
+
+    public String getName() {
+        return this.name;
+    }
 }
+
--- src/test/java/BusTest.java
+++ src/test/java/BusTest.java
@@ -1,2 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BusTest {
+
+    private Bus bus;
+
+    @Before
+    public void before() {
+        bus = new Bus("E39");
+    }
+
+    @Test
+    public void hasName(){
+    assertEquals("E39", bus.getName());
+        }
+
 }
